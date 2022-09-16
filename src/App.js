c247f3b24c892b56810f5f4865c4a18ad2943e62
Sumanta Kabiraj <sumantablog@gmail.com>
2022-09-16 11:40:20 +0530

    initiate app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { NavLink, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header className="header">
+        <div className="logo">Dashboard</div>
       </header>
+      <main className="main">
+        <div className="sidebar-container">
+          <ul>
+            <li>
+              <NavLink to="/" exact>
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/articles" exact>
+                Articles
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/people" exact>
+                People
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/books" exact>
+                Books
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/help" exact>
+                Help & Support
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+        <div className="body-container">
+          <Switch>
+            <Route path="/" exact>
+              <h2>🚀 Welcome to Homepage!</h2>
+            </Route>
+            <Route path="/help">Help</Route>
+            <Route path="/articles" exact>
+              Articles
+            </Route>
+            <Route path="/people">People</Route>
+            <Route path="/books">Books</Route>
+            <Route path="/articles/:slug">Article</Route>
+            <Route path="*">NotFound</Route>
+          </Switch>
+        </div>
+      </main>
     </div>
   );
 }
